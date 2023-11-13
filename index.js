139acaf6cdf0ddc7c95b1c02729d78f15c9c6b91
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-13 22:41:54 +0300

    created function calculateAmount, used methods querySelector and addEventListener

---
+++ index.js
@@ -0,0 +1,9 @@
+const calculate = document.querySelector("#btn"); 
+calculate.addEventListener("click", calculateAmount);
+
+function calculateAmount() {
+const bill = document.querySelector("#bill").value; 
+const people = document.querySelector("#people").value;
+const tip = document.querySelector("#tip").value;
+
+}
