e4a7c2160d86a1109f1e1b60e794cd4343760c73
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-14 15:27:15 +0300

    created variable to show calculation of bill

--- index.js
+++ index.js
@@ -11,5 +11,16 @@ if (bill === "" || people === "") {
     alert("Error!");
 }
 
+// How much 1 person paid
 let amountPerPerson = bill/people;
+
+// How much 1 person will pay tips
+let tipPerPerson = (bill * tip) / people; 
+
+// total amount of bill per person 
+let totalSum = amountPerPerson + tipPerPerson;
+
+document.querySelector("#dividedBill").textContent = amountPerPerson; 
+document.querySelector("#dividedTips").textContent = tipPerPerson;
+document.querySelector("totalBill").textContent = totalSum; 
 }
