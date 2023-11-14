1f35e8202cef1fcd29aa9a54c462a595347f66c4
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-14 15:16:19 +0300

    new variable amount PerPerson and method preventDefault

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const calculate = document.querySelector("#btn"); 
 calculate.addEventListener("click", calculateAmount);
 
-function calculateAmount() {
+function calculateAmount(e) {
+e.preventDefault();
 const bill = document.querySelector("#bill").value; 
 const people = document.querySelector("#people").value;
 const tip = document.querySelector("#tip").value;
@@ -9,4 +10,6 @@ const tip = document.querySelector("#tip").value;
 if (bill === "" || people === "") {
     alert("Error!");
 }
+
+let amountPerPerson = bill/people;
 }
